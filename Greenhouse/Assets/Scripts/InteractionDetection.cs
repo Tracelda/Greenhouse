32c44c1163e69a0002d8d06e84b4c826e2a77271
Edward Goodhew <ejgoodhew@btinternet.com>
2020-09-27 21:57:53 +0100

    Able to detect which table the player is looking at

--- Greenhouse/Assets/Scripts/InteractionDetection.cs
+++ Greenhouse/Assets/Scripts/InteractionDetection.cs
@@ -9,6 +9,13 @@ public class InteractionDetection : MonoBehaviour
     public float MaxDistance;
     public Vector3 Direction;
     public RaycastHit hit;
+    public GameObject[] Benches;
+
+    public enum BenchName
+    {
+       Flowers, CrossBreeding, Bushes, Trees
+    }
+    public BenchName IDNum;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +27,72 @@ public class InteractionDetection : MonoBehaviour
     {
         if (RaycastActive)
         {
-           
-            if(Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), MaxDistance))
+            //DetectInput();
+            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), out hit, MaxDistance))
             {
                 Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.yellow);
                 Debug.Log("Hit");
+                for (int i = 0; i< 4 ; i++)
+                {
+                    Debug.Log(Benches[i] + "aaaa" + i + " Benches length: " + Benches.Length);
+                    
+                    if (Benches[i] == hit.transform.gameObject)
+                    {
+                        IDNum = (BenchName)i;
+                        Debug.Log("IDNUM = " + IDNum);
+                    }
+                }
+            }
+            switch (IDNum)
+            {
+                case BenchName.Flowers:
+                    if (Input.GetButton("Interact"))
+                    {
+                        Debug.Log("Flowers");
+                    }
+                    break;
+
+                case BenchName.CrossBreeding:
+                    if (DetectInput())
+                    {
+                        Debug.Log("CrossBreeding");
+                    }
+                    break;
+
+                case BenchName.Bushes:
+                    if (DetectInput())
+                    {
+                        Debug.Log("Bushes");
+                    }
+                    break;
+
+                case BenchName.Trees:
+                    if (DetectInput())
+                    {
+                        Debug.Log("Trees");
+                    }
+                    break;
             }
         }
         else
         {
 
         }
+    }
+
+    public bool DetectInput()
+    {
+        bool ButtonPressed;
+        if (Input.GetButtonDown("Interact"))
+        {
+            Debug.Log("Interact button hit");
+            ButtonPressed = true;
+        }
+        else
+        {
+            ButtonPressed = false;
+        }
+
+        return ButtonPressed;
     }
 }
