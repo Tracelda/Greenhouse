8eacec15538457276f0b6ca5de44097fe36d26c5
Edward Goodhew <ejgoodhew@btinternet.com>
2020-10-02 20:28:42 +0100

    Plant base scrpt started

---
+++ Greenhouse/Assets/Scripts/PlantBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlantType {Flower, Tree, Bush};
+public enum Maturity {Seed, Sapling, Mature};
+
+[System.Serializable]
+public struct Plant
+{
+    public PlantType plantType;
+    public Maturity maturity;
+    public int TimeTillMature;
+}
+
+public class PlantBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
